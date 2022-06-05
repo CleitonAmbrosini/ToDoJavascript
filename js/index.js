30fa4c221a190b958c7cf93379d72320e0117563
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2022-06-05 20:36:27 -0300

    feat: save and update tasks with browser storage

--- js/index.js
+++ js/index.js
@@ -1,42 +1,63 @@
-const createGridOfTasks = () => {
-  const tasksGrid = document.querySelector("#tasks");
-  const tasksList = document.createElement("ul");
-  tasksGrid.appendChild(tasksList);
-}
+const form = document.querySelector('form');
+const inputField = form.querySelector('input');
+const gridOfTasks = document.querySelector('#tasks');
 
-const toDoJavascript = () => {
-  const form = document.querySelector("#form");
-  const grid = document.querySelector("#tasks");
-  form.addEventListener('submit', handleSubmit);
-  grid.addEventListener('click', handleDelete);
-};
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  const newTask = document.querySelector("#todo").value;
-  saveLocalStorage(newTask);
-}
-
-const saveLocalStorage = (task) => {
-  let id = localStorage.length;
-  localStorage.setItem(id++, task);
-}
-
-const handleDelete = (e) => {
-  localStorage.removeItem(e.target.id);
-  updateTaskList();
-}
-
-const updateTaskList = () => {
-  const tasksList = document.querySelector("#tasks ul");
-  for (let key in localStorage) {
-    const task = document.createElement("li");
-    task.id = key;
-    task.innerHTML = localStorage[key];
-    tasksList.appendChild(task);
+const verifyIfTaskExists = () => {
+  if (!localStorage.getItem('tasks')) return
+  const tasksFromStorage = JSON.parse(localStorage.getItem('tasks'));
+  for (let task of tasksFromStorage) {
+    createGridOfTasks(task);
   }
 }
 
-createGridOfTasks();
-updateTaskList();
-toDoJavascript();
+document.addEventListener('click', (e) => {
+  const element = e.target;
+  if (element.classList.contains('delete-task')) {
+    element.parentElement.remove();
+    saveTasksInStorage();
+  }
+})
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (!getIputValue()) return
+  createGridOfTasks(getIputValue());
+  saveTasksInStorage();
+  inputField.value = '';
+})
+
+const getIputValue = () => {
+  return inputField.value;
+}
+
+const createGridOfTasks = (task) => {
+  gridOfTasks.appendChild(createLi(task));
+}
+
+const createLi = (task) => {
+  const li = document.createElement('li');
+  const paragraph = document.createElement('spam');
+  paragraph.setAttribute('class', 'task-description');
+  paragraph.innerText = task;
+  li.appendChild(paragraph);
+  li.appendChild(createDeleteButton());
+  return li;
+}
+
+const createDeleteButton = () => {
+  const button = document.createElement('button');
+  button.innerText = 'Delete';
+  button.setAttribute('class', 'delete-task');
+  return button;
+}
+
+const saveTasksInStorage = () => {
+  const tasksOfGrid = gridOfTasks.querySelectorAll('.task-description');
+  const tasks = [];
+  for (let task of tasksOfGrid) {
+    tasks.push(task.innerText);
+  }
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+verifyIfTaskExists();
